c0b869496ab5c78dd71f3bf70151959db24405aa
Daniel Schroth <daniel@novissimus.com>
2022-08-22 10:54:15 +0200

    Initial Commit

---
+++ AHASegmentGraphic/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AHASegmentGraphic
+//
+//  Created by Daniel Schroth on 22.08.22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
